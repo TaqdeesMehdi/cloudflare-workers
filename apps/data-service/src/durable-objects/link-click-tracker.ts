652f1d51d9d2a5c80a808ddb0a378d0f74434ee5
TaqdeesMehdi <taqdeesm99@gmail.com>
2026-08-31 19:29:03 +0500

    web socket implementation

--- apps/data-service/src/durable-objects/link-click-tracker.ts
+++ apps/data-service/src/durable-objects/link-click-tracker.ts
@@ -1,12 +1,25 @@
+import { deleteClicksBefore, getRecentClicks } from '@/helpers/durable-queries';
 import { DurableObject } from 'cloudflare:workers';
 import moment from 'moment';
 
 export class LinkClickTracker extends DurableObject {
 	sql: SqlStorage;
+
+	mostRecentOffSetTime: number = 0;
+	leastRecentOffSetTime: number = 0;
+
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
 		this.sql = ctx.storage.sql;
 		ctx.blockConcurrencyWhile(async () => {
+			const [leastRecentOffSetTime, mostRecentOffSetTime] = await Promise.all([
+				ctx.storage.get<number>('leastRecentOffSetTime'),
+				ctx.storage.get<number>('mostRecentOffSetTime'),
+			]);
+
+			this.leastRecentOffSetTime = leastRecentOffSetTime || this.leastRecentOffSetTime;
+			this.mostRecentOffSetTime = mostRecentOffSetTime || this.mostRecentOffSetTime;
+
 			this.sql.exec(`
             CREATE TABLE IF NOT EXISTS geo_link_clicks (
             latitude REAL NOT NULL,
@@ -30,26 +43,40 @@ export class LinkClickTracker extends DurableObject {
 			country,
 			time,
 		);
+
+		const alarm = await this.ctx.storage.getAlarm();
+		if (!alarm) await this.ctx.storage.setAlarm(moment().add(2, 'seconds').valueOf());
+	}
+
+	async alarm() {
+		console.log('Alarm');
+		const clickData = getRecentClicks(this.sql, this.mostRecentOffSetTime);
+		const sockets = this.ctx.getWebSockets();
+		for (const socket of sockets) {
+			socket.send(JSON.stringify(clickData.clicks));
+		}
+		await this.flushOffsetTimes(clickData.mostRecentTime, clickData.oldestTime);
+		await deleteClicksBefore(this.sql, clickData.oldestTime);
+	}
+
+	async flushOffsetTimes(mostRecentOffSetTime: number, leastRecentOffSetTime: number) {
+		this.mostRecentOffSetTime = mostRecentOffSetTime;
+		this.leastRecentOffSetTime = leastRecentOffSetTime;
+		await this.ctx.storage.put('mostRecentOffsetTime', this.mostRecentOffSetTime);
+		await this.ctx.storage.put('leastRecentOffsetTime', this.leastRecentOffSetTime);
 	}
 
 	async fetch(_: Request) {
-		const query = `
-        SELECT * FROM geo_link_clicks
-        limit 100
-        `;
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+		this.ctx.acceptWebSocket(server);
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
+	}
 
-		const cursor = this.sql.exec(query);
-		const result = cursor.toArray();
-
-		return new Response(
-			JSON.stringify({
-				clicks: result,
-			}),
-			{
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			},
-		);
+	webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean): void | Promise<void> {
+		console.log('Websocket connection is closed');
 	}
 }
--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -5,8 +5,16 @@ import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
-App.get('/link-click/:accountId', async (c) => {
-	const accountId = c.req.param('accountId');
+App.get('/click-socket', async (c) => {
+	const upgradeHeader = c.req.header('Upgrade');
+
+	if (!upgradeHeader || upgradeHeader !== 'websocket') {
+		return c.text('Expected Upgrade: websocket', 426);
+	}
+
+	// const accountId = c.req.header('account-id');
+	const accountId = '1234567890';
+	if (!accountId) return c.text('NO header found', 404);
 	const doId = c.env.LINK_CLICK_TRACKER_OBJECT.idFromName(accountId);
 	const stub = c.env.LINK_CLICK_TRACKER_OBJECT.get(doId);
 	return await stub.fetch(c.req.raw);
